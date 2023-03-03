01222cabd90f827d29a00ea565c7e9e52d6ff737
skandal228 <yumakhankov@gmail.com>
2023-03-03 12:14:15 +0300

    Huit123

--- src/HomeWork/Lesson_7_7/HuyVrot.java
+++ src/HomeWork/Lesson_7_7/HuyVrot.java
@@ -6,6 +6,6 @@ public class HuyVrot {
         public static void main(String[] args) {
             HomeWork.Lesson_7.Employee emp2 = new HomeWork.Lesson_7.Employee(12, "Huesosov" );
 
-         //   System.out.println(emp2.id +".Фамилия: "+ emp2.surname); // id ошибка - т.к. access modifier стоит (defoult) Итог: всё правильно!
+         // System.out.println(emp2.id +".Фамилия: "+ emp2.surname); // id ошибка - т.к. access modifier стоит (defoult) Итог: всё правильно!
 }
 }
