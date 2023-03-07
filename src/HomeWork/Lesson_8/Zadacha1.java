d3de62f11d558cb9a36e80942dca30a25d3dd0ea
skandal228 <yumakhankov@gmail.com>
2023-03-07 13:08:11 +0300

    Huit12123

--- src/HomeWork/Lesson_8/Zadacha1.java
+++ src/HomeWork/Lesson_8/Zadacha1.java
@@ -3,19 +3,28 @@ package HomeWork.Lesson_8;
  2ой делит первое число из парметра на второе иничего не возвращает, лишь выводит на дисплей в читабельном виде целое частное и остаток.
  Во втором классе по 2раза используйте данные методы.*/
 public class Zadacha1 {
+    /*1. 3 числа из параметра метода и возвращает их произведение.*/
     public static int umnozhenie  (int a, int b, int c){
         return a*b*c;
     }
+    /* 2ой метод делит первое число из парметра на второе и ничего не возвращает, лишь выводит на дисплей в читабельном виде
+    целое частное и остаток.*/
      static void delenie ( double x, double y){
         System.out.println("Celoye chastnoye :" + x/y);
         System.out.println("Ostatok : " +x%y);
     }
 }
-
+/*Во втором классе по 2раза используйте данные методы. */
 class Zadacha1Test{
     public static void main(String[] args) {
+        /* 1ый раз. */
+
         System.out.println(Zadacha1.umnozhenie(1,2,3));
         Zadacha1.delenie(15,4);
 
+        /* 2ой раз.*/
+
+        System.out.println(Zadacha1.umnozhenie(4,5,6));
+        Zadacha1.delenie(16,9);
     }
 }
