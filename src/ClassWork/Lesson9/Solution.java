6883833cb3b656a7e0f558777fd530f69bb15985
skandal228 <yumakhankov@gmail.com>
2023-03-13 16:53:50 +0300

    Commit 16:53 13.03.2013

---
+++ src/ClassWork/Lesson9/Solution.java
@@ -0,0 +1,12 @@
+package ClassWork.Lesson9;
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+        System.out.println("Строка номер Два");
+        System.out.println("Строка номер Три");
+        System.out.println("Строка номер один");
+
+    }
+}
