42a8f91fe5b012f9d5a3e750bb4e4100cbff693e
skandal228 <yumakhankov@gmail.com>
2023-04-03 16:02:14 +0300

    Commit 16:14 03.04.2023

---
+++ src/ClassWork/For/Zadacha2.java
@@ -0,0 +1,29 @@
+package ClassWork.For;
+import javax.swing.*;
+import java.util.Scanner;
+/*В методе main с клавиатуры считывается 3 целых числа: start, end (start <= end), multiple.
+Допиши программу, чтобы на экран выводилась сумма чисел от start (включительно) до end (не включительно), кратных multiple.
+Для этого используй цикл for.
+Подсказка: чтобы перейти к следующей итерации цикла, используй оператор continue.
+Требования!!!
+* Программа должна вывести в консоль сумму чисел от start (включительно) до end (не включительно), кратных multiple.
+* В программе не обходимо использовать цикл for
+* В цикле for необходимо использовать оператор continue. */
+
+public class Zadacha2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+        int multiple = scanner.nextInt();
+
+        int sum = 0;
+        for (int i = start;i<end;i++){
+           if (i%multiple !=0){
+               continue;
+           }
+           sum = sum +i;
+        }
+System.out.println(sum);
+    }
+}
